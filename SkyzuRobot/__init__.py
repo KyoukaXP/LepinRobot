522a09976c1512dd9f25e46f71bec243427196bb
Yansen <97857681+Yansensad@users.noreply.github.com>
2022-03-09 17:46:00 +0700

    test

--- SkyzuRobot/__init__.py
+++ SkyzuRobot/__init__.py
@@ -8,7 +8,6 @@ import spamwatch
 import telegram.ext as tg
 from inspect import getfullargspec
 from aiohttp import ClientSession
-from Python_ARQ import ARQ
 from telethon import TelegramClient
 from telethon.sessions import StringSession
 from telethon.sessions import MemorySession
