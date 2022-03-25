6f192ba960b82abe2b5f75a8835cbb09ccaef344
Yansen <97857681+KyoukaXP@users.noreply.github.com>
2022-03-25 16:07:26 +0700

    Update userinfo.py

--- SkyzuRobot/modules/userinfo.py
+++ SkyzuRobot/modules/userinfo.py
@@ -464,7 +464,7 @@ def set_about_me(update: Update, context: CallbackContext):
 
 @sudo_plus
 def stats(update: Update, context: CallbackContext):
-    stats = "<b>╔═━「 Current Lepin Statistics 」</b>\n" + "\n".join(
+    stats = "<b>╔═━「 Current ᴋʏᴏᴜᴋᴀ Statistics 」</b>\n" + "\n".join(
         [mod.__stats__() for mod in STATS]
     )
     result = re.sub(r"(\d+)", r"<code>\1</code>", stats)
